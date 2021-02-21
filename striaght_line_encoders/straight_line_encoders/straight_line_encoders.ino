faeec92a0c2ec0e6ac28ce23efd3df20b718d68b
Andrew Nguyen <aln96@pitt.edu>
2021-02-21 15:45:03 -0500

    Implemented push button activation,buzzer,motor activation, and moving forward

--- striaght_line_encoders/straight_line_encoders/straight_line_encoders.ino
+++ striaght_line_encoders/straight_line_encoders/straight_line_encoders.ino
@@ -1,15 +1,32 @@
+
 #include <Wire.h>
 #include <Zumo32U4.h>
 
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4Buzzer buzzer;
 
+// Initial speeds for left and right motors (0-400)
+int16_t initialMotorSpeedLeft  = 150;
+int16_t initialMotorSpeedRight = 150;
 
+void setup() { 
+  buttonA.waitForButton(); // Wait for button A to be pressed to start
 
-void setup() {
-  // put your setup code here, to run once:
+  // Buzzer variables in case they need to be changed
+  unsigned int buzzerFrequency = 261; // Middle C
+  unsigned int buzzerDuration =  50; // In milliseconds
+  unsigned char buzzerVolume = 10; // On scale of 0-15
+  
+  buzzer.playFrequency(buzzerFrequency, buzzerDuration, buzzerVolume); // Play buzzer
 
+  unsigned long initialDelay = 1000;
+  delay(initialDelay); // Delay robot so it doesn't immediately move after pressing button A
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  motors.setSpeeds(initialMotorSpeedLeft, initialMotorSpeedRight); // Run motors at specified speeds
 
 }
