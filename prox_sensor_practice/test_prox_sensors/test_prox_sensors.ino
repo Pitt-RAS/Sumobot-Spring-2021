b5c9d18d4698515d7fb64f6013476cdd2d3b5f75
NMMallick <79054723+NMMallick@users.noreply.github.com>
2021-02-22 19:43:35 -0500

    Practice reading prox sensors and displaying to LCD + using buzzer

---
+++ prox_sensor_practice/test_prox_sensors/test_prox_sensors.ino
@@ -0,0 +1,47 @@
+#include <Wire.h> 
+#include <Zumo32U4.h> 
+
+Zumo32U4Buzzer buzzer;
+Zumo32U4LCD lcd; 
+Zumo32U4ProximitySensors proxSensors;
+Zumo32U4ButtonA buttonA; 
+
+  // Buzzer variables in case they need to be changed
+  unsigned int buzzerFrequency = 261; // Middle C
+  unsigned int buzzerDuration =  50; // In milliseconds
+  unsigned char buzzerVolume = 8; // On scale of 0-15
+
+void setup() {
+  // put your setup code here, to run once:
+  proxSensors.initThreeSensors(); 
+  buttonA.waitForButton(); // Wait for button A to be pressed to start 
+}
+
+
+void loop() {
+  /*Read Sensors*/
+  proxSensors.read(); 
+  int sensors[4];
+  sensors[0] = proxSensors.countsLeftWithLeftLeds();
+  sensors[1] = proxSensors.countsFrontWithLeftLeds();
+  sensors[2] = proxSensors.countsFrontWithRightLeds();
+  sensors[3] = proxSensors.countsRightWithRightLeds();
+
+  /*LCD Display, to values of each sensor*/
+  lcd.gotoXY(0, 1);
+  lcd.print(sensors[0]);
+  lcd.print(" "); 
+  lcd.print(sensors[1]);
+  lcd.print(" "); 
+  lcd.print(sensors[2]);
+  lcd.print(" "); 
+  lcd.print(sensors[3]);
+  lcd.print(" "); 
+
+  for(int i = 0; i < 4; i++) {
+    if(sensors[i] >= 5) {
+        buzzer.playFrequency(buzzerFrequency, buzzerDuration, buzzerVolume); // Play buzzer
+      }
+    }
+  
+}
