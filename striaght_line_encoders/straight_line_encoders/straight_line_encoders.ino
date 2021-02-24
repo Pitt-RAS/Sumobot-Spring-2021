075f709727534c9992db4343479463dbe703e044
ethan tost <emt84@pitt.edu>
2021-02-23 19:07:38 -0500

    implemented encoder reading and straightening

--- striaght_line_encoders/straight_line_encoders/straight_line_encoders.ino
+++ striaght_line_encoders/straight_line_encoders/straight_line_encoders.ino
@@ -5,28 +5,58 @@
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4Buzzer buzzer;
+Zumo32U4Encoders encoders;
 
 // Initial speeds for left and right motors (0-400)
-int16_t initialMotorSpeedLeft  = 150;
-int16_t initialMotorSpeedRight = 150;
+int16_t motorSpeedLeft  = 150;
+int16_t motorSpeedRight = 150;
 
-void setup() { 
+//declare for use in encoder adjustment
+unsigned long int lastEncoderTime;
+
+void setup() {
   buttonA.waitForButton(); // Wait for button A to be pressed to start
 
   // Buzzer variables in case they need to be changed
   unsigned int buzzerFrequency = 261; // Middle C
   unsigned int buzzerDuration =  50; // In milliseconds
   unsigned char buzzerVolume = 10; // On scale of 0-15
-  
+
   buzzer.playFrequency(buzzerFrequency, buzzerDuration, buzzerVolume); // Play buzzer
 
   unsigned long initialDelay = 1000;
   delay(initialDelay); // Delay robot so it doesn't immediately move after pressing button A
 
-  
+  lastEncoderTime = millis(); //sets initial value
 }
 
 void loop() {
-  motors.setSpeeds(initialMotorSpeedLeft, initialMotorSpeedRight); // Run motors at specified speeds
+  motors.setSpeeds(motorSpeedLeft, motorSpeedRight); // Run motors at specified speeds
+
+  //reads encoders and adjusts speeds every 100 ms
+  if ((millis() - lastEncoderTime) >= 100)
+  {
+    lastEncoderTime = millis();
+    int16_t countsLeft = encoders.getCountsAndResetLeft();
+    int16_t countsRight = encoders.getCountsAndResetRight();
+
+    //adjusts speed based on encoder data
+    if(countsLeft < countsRight){
+      motorSpeedLeft += 1;
+      motorSpeedRight -= 1;
+    } else if (countsLeft > countsRight){
+      motorSpeedLeft -= 1;
+      motorSpeedRight += 1;
+    }
+    Serial.print(countsLeft);
+    Serial.print("\t");
+    Serial.print(countsRight);
+    Serial.print("\t");
+    Serial.print(motorSpeedLeft);
+    Serial.print("\t");
+    Serial.print(motorSpeedRight);
+    Serial.print("\n");
+
+  }
 
 }
