e5f42cffe207b6372f1120b161e34eef12bb65b0
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-03-02 23:05:11 -0500

    fixing capitalization of constant variables, adding error_correction method comment, initializing additional variables in setPID

--- straight_line_encoders/straight_line_encoders/PID.cpp
+++ straight_line_encoders/straight_line_encoders/PID.cpp
@@ -1,6 +1,22 @@
 #include "Arduino.h"
 #include "PID.h"
 
+//Set all K values and reset error and timing fields
+void PID::setPID(float Kp_new, float Ki_new, float Kd_new)
+{
+	//Update K values
+	Kp = Kp_new;
+	Ki = Ki_new;
+	Kd = Kd_new;
+
+	// Initialize lastTime, Ei, and lastEp
+	lastTime = millis();
+	Ei = 0;
+	lastEp = 0;
+}
+
+// use the PID controller to generate a control signal by sending the controller
+// a desired set_point and the current feedback state of the system
 float PID::error_correction(float set_point, float input)
 {
 	// determine time elapsed since last error calculation and update lastTime
@@ -15,14 +31,5 @@ float PID::error_correction(float set_point, float input)
 	lastEp = Ep;
 
 	// calculate output and return
-	return kp * Ep + ki * Ei + kd * Ed;
-}
-
-//Set all K values
-void PID::setPID(float Kp_new, float Ki_new, float Kd_new)
-{
-	//Update K values
-	Kp = Kp_new;
-	Ki = Ki_new;
-	Kd = Kd_new;
+	return Kp * Ep + Ki * Ei + Kd * Ed;
 }
