bc94d1e784a5cccb963e230a1a477bfca4b3a101
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-03-06 18:14:47 -0500

    adding documentation comments

--- straight_line_encoders/straight_line_encoders/src/PID/PID.cpp
+++ straight_line_encoders/straight_line_encoders/src/PID/PID.cpp
@@ -1,10 +1,19 @@
+/*
+ File:		PID.h
+ Created:	3/1/2021
+ Updated:   3/6/2021
+ Author:	Ethan Markowski, Hudson Panning, Brian Randall
+
+ Purpose:   PID controller library
+*/
+
 #include "Arduino.h"
 #include "PID.h"
 
-//Set all K values and reset error and timing fields
+// Set all K values and reset error and timing fields
 void PID::setPID(float Kp_new, float Ki_new, float Kd_new)
 {
-	//Update K values
+	// Update K values
 	Kp = Kp_new;
 	Ki = Ki_new;
 	Kd = Kd_new;
@@ -15,21 +24,21 @@ void PID::setPID(float Kp_new, float Ki_new, float Kd_new)
 	lastEp = 0;
 }
 
-// use the PID controller to generate a control signal by sending the controller
+// Use the PID controller to generate a control signal by sending the controller
 // a desired set_point and the current feedback state of the system
 float PID::error_correction(float set_point, float input)
 {
-	// determine time elapsed since last error calculation and update lastTime
+	// Determine time elapsed since last error calculation and update lastTime
 	unsigned long currentTime = millis();
 	unsigned long elapsedTime = currentTime - lastTime;
 	lastTime = currentTime;
 
-	// error calculations
+	// Error calculations
 	Ep = set_point - input;
 	Ei += Ep * elapsedTime;
 	Ed = (Ep - lastEp) / elapsedTime;
 	lastEp = Ep;
 
-	// calculate output and return
+	// Calculate output and return
 	return Kp * Ep + Ki * Ei + Kd * Ed;
 }
