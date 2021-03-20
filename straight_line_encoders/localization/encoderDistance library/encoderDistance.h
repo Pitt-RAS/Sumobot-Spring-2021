8adfec61c2e4049c0dff93b9d34ff6b064ab40e4
NMMallick <79054723+NMMallick@users.noreply.github.com>
2021-03-20 13:27:50 -0400

    encoderDistance library

---
+++ straight_line_encoders/localization/encoderDistance library/encoderDistance.h
@@ -0,0 +1,22 @@
+#ifndef ENCODERDISTANCE_H
+#define ENCDOERDISTANCE_H
+
+
+
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+class encoderDistance {
+    public:
+
+
+    private:
+    
+
+}; 
+
+
+
+
+
+#endif
