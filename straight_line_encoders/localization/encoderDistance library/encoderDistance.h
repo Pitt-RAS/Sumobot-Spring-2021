019487d6c99c418aac706238ab487d4cd942f0b1
NMMallick <79054723+NMMallick@users.noreply.github.com>
2021-03-20 13:38:31 -0400

    libary work

--- straight_line_encoders/localization/encoderDistance library/encoderDistance.h
+++ straight_line_encoders/localization/encoderDistance library/encoderDistance.h
@@ -8,15 +8,19 @@
 
 class encoderDistance {
     public:
-
-
+        unsigned long distanceCentimeters;
+        
     private:
-    
+        encoderDistance();
+        encoderDistance(int16_t);
+
+        /*getter methods for distance values (cm and in) */
+        unsigned long getDistanceCentimeters() const;
+        
+        /*setters methods for distance values*/
+        void setDistanceCentimeters();
+        
 
 }; 
-
-
-
-
 
 #endif
