774a59b08d325d67577ed8525591e067f8db6639
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-03-28 14:24:37 -0400

    making folder for state machine implementation

---
+++ CombinedCode_StateMachine/src/PID/PID.cpp
@@ -0,0 +1,44 @@
+/*
+ File:		PID.h
+ Created:	3/1/2021
+ Updated:	3/6/2021
+ Author:	Ethan Markowski, Hudson Panning, Brian Randall
+
+ Purpose:	PID controller library
+*/
+
+#include "Arduino.h"
+#include "PID.h"
+
+// Set all K values and reset error and timing fields
+void PID::setPID(float Kp_new, float Ki_new, float Kd_new)
+{
+	// Update K values
+	Kp = Kp_new;
+	Ki = Ki_new;
+	Kd = Kd_new;
+
+	// Initialize lastTime, Ei, and lastEp
+	lastTime = millis();
+	Ei = 0;
+	lastEp = 0;
+}
+
+// Use the PID controller to generate a control signal by sending the controller
+// a desired set_point and the current feedback state of the system
+float PID::error_correction(float set_point, float input)
+{
+	// Determine time elapsed since last error calculation and update lastTime
+	unsigned long currentTime = millis();
+	unsigned long elapsedTime = currentTime - lastTime;
+	lastTime = currentTime;
+
+	// Error calculations
+	Ep = set_point - input;
+	Ei += Ep * elapsedTime;
+	Ed = (Ep - lastEp) / elapsedTime;
+	lastEp = Ep;
+
+	// Calculate output and return
+	return Kp * Ep + Ki * Ei + Kd * Ed;
+}
---
+++ CombinedCode_StateMachine/src/PID/PID.h
@@ -0,0 +1,39 @@
+/*
+ File:      PID.h
+ Created:   3/1/2021
+ Updated:   3/6/2021
+ Author:    Ethan Markowski, Hudson Panning, Brian Randall
+ 
+ Purpose:   PID controller library
+*/
+
+#ifndef PID_h
+#define PID_h
+
+#include "Arduino.h"
+
+class PID
+{
+    float Kp;
+    float Ki;
+    float Kd;
+    unsigned long lastTime;
+    float Ep;
+    float Ei;
+    float Ed;
+    float lastEp;
+
+  public:
+      // Set all K values and reset error and timing fields
+      // ** The error_correction method should not be used without first calling setPID
+      // ** If the robot is sitting in a paused state, setPID should be used on resume to reset the PID object's timer
+      // Failing to do this could result in an arbitrarily large amount of integral error being stored on the
+      // control loop following the exit from the paused state
+      void setPID(float Kp_new, float Ki_new, float Kd_new);
+
+      // Calling error_correction uses the PID object to generate a control signal by sending the controller a desired
+      // set_point and the current feedback state of the system
+      float error_correction(float set_point, float input);
+};
+
+#endif
