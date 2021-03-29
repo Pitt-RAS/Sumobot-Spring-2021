26ad620e5d2960c5fac5e3e9bf590067d5cd3d1d
lumpia2 <cody.cacal@yahoo.com>
2021-03-28 21:05:19 -0400

    Added return to origin function WIP

--- localization/Cody/encoderDistance library/encoderDistance.cpp
+++ localization/Cody/encoderDistance library/encoderDistance.cpp
@@ -1,9 +1,37 @@
 #include "encoderDistance.h"
+#include <stdint.h>
 
+encoderDistance::encoderDistance(){
+    encoder = 0;
+}
+encoderDistance::encoderDistance(int16_t encoder){
+    this -> encoder;
+}
 unsigned long encoderDistance::getDistance() const{
     return distance;
 }
 
 void encoderDistance::setDistance(){
-    distance = (encoder/909.7)*12;
+    distance = (encoder/909.7)*3.81;
+}
+
+void encoderDistance::returnToOrigin(){
+    //turnAround
+    int16_t originEncoder = (getDistance()/3.81)*909.7;
+    //motors.encoder(originEncoder)
+
+}
+
+int32_t encoderDistance::getAngle() {
+    return (((int32_t)turnAngle >> 16)*360)>>16; 
+
+}
+
+int32_t encoderDistance::turnAround(){
+    Zumo32U4Motors motors;
+
+    int16_t motorSetSpeedLeft = 200;
+    int16_t motorSetSpeedRight = 200;
+
+    if(getAngle() < )
 }
--- localization/Cody/encoderDistance library/encoderDistance.h
+++ localization/Cody/encoderDistance library/encoderDistance.h
@@ -5,22 +5,32 @@
 
 #include <Wire.h>
 #include <Zumo32U4.h>
+#include "TurnSensor.h"
 #include <iostream>
+#include <stdint.h>
 
 class encoderDistance{
     private:
         unsigned long distance;
         int16_t encoder;
+
+        int32_t getAngle();
+        int32_t turnAround();
         
     public:
         encoderDistance();
         encoderDistance(int16_t);
 
-        /*getter methods for distance values (cm and in) */
+        /* getter methods for distance values (cm) */
         unsigned long getDistance() const;
         
-        /*setters methods for distance values*/
+        /* setters methods for distance values */
         void setDistance();
+
+        /* returns bot to its original position */
+        /* only works in a straight line for now */
+        void returnToOrigin();
+
         
 
 };
